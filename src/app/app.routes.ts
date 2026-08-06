7247b0d0f3623295fc5eba99b9205c55bcb84b99
Gabriel <contato.gabrielportari@gmail.com>
2026-08-06 15:16:15 -0300

    feat: scaffold storefront/admin route components and shared header/footer layout (roadmap 5.1, 5.5)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./storefront/product-list/product-list').then((m) => m.ProductList)
+  },
+  {
+    path: 'produtos/:id',
+    loadComponent: () => import('./storefront/product-detail/product-detail').then((m) => m.ProductDetail)
+  },
+  {
+    path: 'carrinho',
+    loadComponent: () => import('./storefront/cart/cart').then((m) => m.Cart)
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./storefront/checkout/checkout').then((m) => m.Checkout)
+  },
+  {
+    path: 'pedido/:id',
+    loadComponent: () =>
+      import('./storefront/order-confirmation/order-confirmation').then((m) => m.OrderConfirmation)
+  },
+  {
+    path: 'admin/login',
+    loadComponent: () => import('./admin/login/login').then((m) => m.Login)
+  },
+  {
+    // TODO(Fase 7.2): proteger com authGuard quando ele for criado
+    path: 'admin/produtos',
+    loadComponent: () =>
+      import('./admin/product-management/product-management').then((m) => m.ProductManagement)
+  },
+  {
+    // TODO(Fase 7.2): proteger com authGuard quando ele for criado
+    path: 'admin/categorias',
+    loadComponent: () =>
+      import('./admin/category-management/category-management').then((m) => m.CategoryManagement)
+  },
+  {
+    // TODO(Fase 7.2): proteger com authGuard quando ele for criado
+    path: 'admin/pedidos',
+    loadComponent: () => import('./admin/order-list/order-list').then((m) => m.OrderList)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/storefront/order-confirmation/order-confirmation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-confirmation',
+  imports: [],
+  templateUrl: './order-confirmation.html',
+  styleUrl: './order-confirmation.scss',
+})
+export class OrderConfirmation {}
